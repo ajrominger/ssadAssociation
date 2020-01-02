2ce30ac61f3c43e8d535728c1cded8b1b6e85762
ajrominger <ajrominger@gmail.com>
2020-01-02 09:49:48 -0700

    added help doc on abundance data

---
+++ R/abundance_matrices.R
@@ -0,0 +1,15 @@
+#' Assemblage data from Calatayud et al.
+#'
+#' A list of site by species matrices, each containing counts or percent
+#' covers of different species observed across locations
+#'
+#' @format A list of 385 assemblages, each represented by a site by species matrix with
+#' \describe{
+#'   \item{rows}{representing sites}
+#'   \item{columns}{representing species}
+#'   \item{cells}{recording the abundance or percent cover of each species in each site}
+#' }
+#'
+#' @references Calatayud, J., et al. (2019). Positive associations among rare species and their persistence in ecological assemblages. Nature Ecology & Evolution, 1-6.
+#' @source \url{https://figshare.com/articles/Positive_associations_among_rare_species_and_their_persistence_in_ecological_assemblages/9906092}
+'abundMats'
