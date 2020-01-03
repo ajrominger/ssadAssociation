cbffb17724851b4a1e7679c32de6e50f53c3cd99
ajrominger <ajrominger@gmail.com>
2020-01-03 11:38:30 -0700

    got test working with new input for `simPlusMinus` and new output from `plusMinus`

--- tests/testthat/test_simPlusMinus.R
+++ tests/testthat/test_simPlusMinus.R
@@ -1,16 +1,17 @@
 context('simulating plusMinus works without error')
 
 nsim <- 2
+testStats <- data.frame(mod = rep('fish', 2), par1 = rep(0.01, 2),
+                        par2 = rep(NA, 2), stringsAsFactors = FALSE,
+                        J = rep(500, 2), nspp = rep(100, 2), nsite = rep(20, 2))
 
 test_that('simPlusMinus returns the correct dimension without error', {
-    x <- simPlusMinus(nsitenspp = data.frame(nsite = rep(20, 2), nspp = rep(50, 2)),
-                      sadStats = data.frame(mod = rep('fish', 2), par1 = rep(0.01, 2),
-                                            par2 = rep(NA, 2)),
+    x <- simPlusMinus(sadStats = testStats,
                       mcCores = 1, ssadType = 'nbinom',
-                      kfun = function(nspp, abund) {return(0.5)}, nsim = 2)
+                      kfun = function(nspp, abund) {return(0.5)}, nsim = nsim)
 
     expect_equal(nsim, nrow(x))
-    expect_equal(10, ncol(x))
+    expect_equal(13, ncol(x))
 })
 
 
@@ -21,5 +22,5 @@ test_that('simpleSim returns the correct dimension without error', {
                    nsim = nsim)
 
     expect_equal(nsim, nrow(x))
-    expect_equal(10, ncol(x))
+    expect_equal(13, ncol(x))
 })
